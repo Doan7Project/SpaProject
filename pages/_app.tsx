a69a633bdf96779b1e992981fd33d2d67e11b04f
Doanftpaptech <doananhphamuk@gmail.com>
2022-08-16 16:47:19 +0700

    date16

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
 import HeaderConponent from "./src/components/header";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+    
       <HeaderConponent/>
+
       <Component {...pageProps} />
     </>
   );
